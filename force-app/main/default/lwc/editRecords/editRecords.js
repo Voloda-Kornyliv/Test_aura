03fecd95eec2db5432966047d0480ccf9d75baba
Voloda-Kornyliv <62346620+Voloda-Kornyliv@users.noreply.github.com>
2021-09-16 14:26:58 +0300

    Create editRecords.js

---
+++ force-app/main/default/lwc/editRecords/editRecords.js
@@ -0,0 +1,16 @@
+import { LightningElement, api } from 'lwc';
+import { NavigationMixin } from 'lightning/navigation';
+
+export default class EditRecords extends NavigationMixin(LightningElement) {
+    @api recordId;
+    handleSuccess(event) {
+        this[NavigationMixin.Navigate]({
+            type: 'standard__recordPage',
+            attributes: {
+                recordId: this.recordId,
+                objectApiName: 'Customer_Report__c',
+                actionName: 'view'
+            },
+        });
+    }
+}
