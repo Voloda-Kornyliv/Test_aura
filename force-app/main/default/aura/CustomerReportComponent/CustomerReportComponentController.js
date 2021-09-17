63bda2b86664ed9eb55da67dadbcb664fe7ba186
Voloda-Kornyliv <62346620+Voloda-Kornyliv@users.noreply.github.com>
2021-09-17 16:43:47 +0300

    Update CustomerReportComponentController.js

--- force-app/main/default/aura/CustomerReportComponent/CustomerReportComponentController.js
+++ force-app/main/default/aura/CustomerReportComponent/CustomerReportComponentController.js
@@ -1,17 +1,24 @@
 ({
     handleSuccess : function(component, event, helper) {
-        component.find('notifLib').showToast({
+        var toastEvent = $A.get("e.force:showToast");
+        toastEvent.setParams({
             "variant": "success",
             "title": "Customer_Report__c",
             "message": "Record ID: " + event.getParam("id")
         });
+        toastEvent.fire();
+        //Return to view records
         var urlEvent = $A.get("e.force:navigateToURL");
+        let urlString = window.location.href;
+        let baseURL = urlString.substring(0, urlString.indexOf("/s"));
         urlEvent.setParams({
-          "url": "https://resilient-goat-k8vmzv-dev-ed.lightning.force.com/lightning/o/Customer_Report__c/list?filterName=Recent"
+          "url": baseURL+"/lightning/o/Customer_Report__c/list?filterName=Recent"
         });
         urlEvent.fire();
-
-
-
+    },
+    handleLoad: function (cmp, event, helper) {
+        var record = event.getParam("recordUi");
+        var fieldNames = Customer_Report__c.keys(record.fields);
+        component.set("v.fields", fieldNames);
     }
 })
