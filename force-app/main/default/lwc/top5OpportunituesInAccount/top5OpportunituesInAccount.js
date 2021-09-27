c252dea081598a755ead30d22c5647f2fd00b82f
Voloda-Kornyliv <62346620+Voloda-Kornyliv@users.noreply.github.com>
2021-09-27 11:56:41 +0300

    Update top5OpportunituesInAccount.js

--- force-app/main/default/lwc/top5OpportunituesInAccount/top5OpportunituesInAccount.js
+++ force-app/main/default/lwc/top5OpportunituesInAccount/top5OpportunituesInAccount.js
@@ -5,7 +5,7 @@ import CLOSEDATE_FIELD from '@salesforce/schema/Opportunity.CloseDate';
 import AMOUNT_FIELD from '@salesforce/schema/Opportunity.Amount';
 import getOpportunity from "@salesforce/apex/GetTop5Opportynitues.getOpportunity";
 const COLUMNS = [
-    { label: 'Opportynitu Name', fieldName: NAME_FIELD.fieldApiName, type: 'text' },
+    { label: 'Opportynitu Name', fieldName: 'recordLink',  type: 'url', typeAttributes:{label:{fieldName: 'Name', target: '_blank'}} },
     { label: 'Stage name', fieldName: STAGENAME_FIELD.fieldApiName, type: 'text' },
     { label: 'Close date', fieldName: CLOSEDATE_FIELD.fieldApiName, type: 'text' },
     { label: 'Amount', fieldName: AMOUNT_FIELD.fieldApiName, type: 'currency' }
@@ -13,6 +13,21 @@ const COLUMNS = [
 export default class Top5OpportunituesInAccount extends LightningElement {
     columns = COLUMNS;
     @api recordId;
-    @wire(getOpportunity, {recordIdAccount: '$recordId'}) Opportynitues;
+    @track opportynitues;
+    @wire(getOpportunity, {recordIdAccount: '$recordId'})
+    GetOpps({ error, data }){
+        if (data){
+            var tempOppList = []; 
+            for (var i = 0; i < data.length; i++) {
+                let tempRecord = Object.assign({}, data[i]);
+                tempRecord.recordLink = '/lightning/r/Opportunity/' + tempRecord.Id + '/view';
+                tempOppList.push(tempRecord); 
+            }
+            this.opportynitues = tempOppList; 
+        }
+        else if(error){
+            console.log(error);
+        }
+     }
     
 }
