0a223e133d389dea2e3bc12377625e17c977de38
Voloda-Kornyliv <62346620+Voloda-Kornyliv@users.noreply.github.com>
2021-10-09 21:48:20 +0300

    Update weatherSettings.js

--- force-app/main/default/lwc/weatherSettings/weatherSettings.js
+++ force-app/main/default/lwc/weatherSettings/weatherSettings.js
@@ -1,3 +1,25 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, wire, track } from 'lwc';
+import getWeatherSetting from "@salesforce/apex/WeatherApp.getWeatherSetting";
+import { ShowToastEvent } from 'lightning/platformShowToastEvent';
 
-export default class WeatherSettings extends LightningElement {}
+export default class WeatherSettings extends LightningElement {
+    @track recordId;
+    @wire(getWeatherSetting)
+    getWeathers({ error, data }){
+        if (data){
+            this.recordId = data.Id;
+        }
+        else if(error){
+            console.log(error);
+        }
+    }
+    handleSuccess() {
+        const event = new ShowToastEvent({
+            title: 'Success',
+            message: 'Setting saved',
+            variant: 'success',
+            mode: 'dismissable'
+        });
+        this.dispatchEvent(event);
+    }
+}
