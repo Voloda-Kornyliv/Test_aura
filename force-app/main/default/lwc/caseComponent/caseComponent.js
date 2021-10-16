2857d9bfdc68c64f567c4207e6ea835a1a2406d3
Voloda-Kornyliv <62346620+Voloda-Kornyliv@users.noreply.github.com>
2021-10-16 20:03:58 +0300

    Update caseComponent.js

--- force-app/main/default/lwc/caseComponent/caseComponent.js
+++ force-app/main/default/lwc/caseComponent/caseComponent.js
@@ -1,3 +1,39 @@
-import { LightningElement } from 'lwc';
+import { LightningElement,track, wire } from 'lwc';
+import GetCaseByDateCreate from '@salesforce/apex/CaseComponent.GetCaseByDateCreate';
 
-export default class CaseComponent extends LightningElement {}
+const COLUMNS = [
+    { label: 'CaseNumber', fieldName: 'CaseNumber', type:'text' },
+    { label: 'CreatedDate', fieldName: 'CreatedDate',type: 'date' },
+    { label: 'Status', fieldName: 'Status', type:'text' },
+    {
+        type: "button",
+        fixedWidth: 150,
+        typeAttributes: {
+            label: 'View Details',
+            title: 'View Details',
+            name: 'viewDetails',
+            value: 'id',
+            variant: 'brand',
+            class: 'scaled-down'
+        }
+    },
+];
+export default class CaseComponent extends LightningElement {
+    @track listCase;
+    columns = COLUMNS;
+    @track recordId;
+    @wire(GetCaseByDateCreate)
+    getCase({ error, data }){
+        if (data){
+            this.listCase = data; 
+            this.recordId = data[0].Id;
+        }
+        else if(error){
+            console.log(error);
+        }
+    }
+    changeRecordId(event){
+        this.recordId = event.detail.row.Id;
+    }
+
+}
