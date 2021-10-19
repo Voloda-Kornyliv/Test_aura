ef6f1b1f8eeb40d58255ee2d39245d0f68ffa60c
Voloda-Kornyliv <62346620+Voloda-Kornyliv@users.noreply.github.com>
2021-10-19 13:52:27 +0300

    Update createAccounts.js

--- force-app/main/default/lwc/createAccounts/createAccounts.js
+++ force-app/main/default/lwc/createAccounts/createAccounts.js
@@ -1,3 +1,13 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, track } from 'lwc';
 
-export default class CreateAccounts extends LightningElement {}
+export default class CreateAccounts extends LightningElement {
+    @track arr = [1];
+    handleadd(){
+        this.arr.push(this.arr.length + 1);
+    }
+    handleremove(){
+        if(this.arr.length > 1){
+            this.arr.pop();
+        }
+    }
+}
