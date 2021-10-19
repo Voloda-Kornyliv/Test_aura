35a3568d08df89513f17cd2e409b3d8bc4f1b00d
Voloda-Kornyliv <62346620+Voloda-Kornyliv@users.noreply.github.com>
2021-10-19 13:52:30 +0300

    Update inputAccountDetalias.js

--- force-app/main/default/lwc/inputAccountDetalias/inputAccountDetalias.js
+++ force-app/main/default/lwc/inputAccountDetalias/inputAccountDetalias.js
@@ -1,3 +1,21 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, api } from 'lwc';
 
-export default class InputAccountDetalias extends LightningElement {}
+export default class InputAccountDetalias extends LightningElement {
+    @api numrow;
+    @api recordData;
+    get showPlus(){
+        if(this.numrow == 1)
+            return true;
+        return false;
+    }
+    addClick(){
+        this.dispatchEvent(new CustomEvent('add'));
+    }
+    removeClick(){
+        this.dispatchEvent(new CustomEvent('remove'));
+    }
+    handleChnage(){
+        
+        this.dispatchEvent(new CustomEvent('changedata'));
+    }
+}
